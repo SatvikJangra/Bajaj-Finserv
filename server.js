48fa2685784cef94cd6af42fbc72c66eb64bc3c5
vikash yadav <vikas.rsry@gmail.com>
2024-03-18 12:59:34 +0530

    update

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Route: POST /bfhl
+app.post('/bfhl', (req, res) => {
+  try {
+    const data = req.body.data;
+    const userId = generateUserId("Yadav", "Vikash Ramkailash","07082003");
+    const email = "yadav3878.be21@chitkara.edu.in";
+    const rollNumber = "2110993878";
+    
+    const evenNumbers = [];
+    const oddNumbers = [];
+    const alphabets = [];
+
+    for (let item of data) {
+      if (!isNaN(item)) {
+        const num = parseInt(item);
+        if (num % 2 === 0) {
+          evenNumbers.push(num.toString());
+        } else {
+          oddNumbers.push(num.toString());
+        }
+      } else if (typeof item === 'string') {
+        alphabets.push(item.toUpperCase());
+      }
+    }
+
+    const response = {
+      is_success: true,
+      user_id: userId,
+      email: email,
+      roll_number: rollNumber,
+      even_numbers: evenNumbers,
+      odd_numbers: oddNumbers,
+      alphabets: alphabets
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Generate User ID function
+function generateUserId(firstName, lastName, dob) {
+  return `${firstName}_${lastName}_${dob}`;
+}
+
+// Start the server
+app.listen(PORT, () => {
+  console.log("Server is running on port ${PORT}");
+});
